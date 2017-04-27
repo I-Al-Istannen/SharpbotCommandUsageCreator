ba284fb10fe0aaf3e8b97744f92fda409e1294a5
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2017-04-27 22:52:13 +0200

    Made it runnable with a normal command
    * Change formatter to be customizable via command line
    
    * Change formatter to support category headers

--- sharpbotcommandusagecreator/outputformatter.py
+++ sharpbotcommandusagecreator/outputformatter.py
@@ -2,16 +2,32 @@ import re
 
 
 class Formatter(object):
-    def __init__(self, command_format):
-        self._command_format = command_format
+    default_format = """### `{name}`
+*Description:* `{description}`
+*Usage:* `{usage}`
+<if>*Credits:* `{credits}`
+*Category:* `{category}`"""
 
-    @classmethod
-    def create_default(cls):
-        return Formatter("""### `{name}`  
-*Description:* `{description}`  
-*Usage:* `{usage}`  
-<if>*Credits:* `{credits}`  
-*Category:* `{parent_folder}`  """)
+    default_format_no_category = """### `{name}`
+*Description:* `{description}`
+*Usage:* `{usage}`
+<if>*Credits:* `{credits}`"""
+
+    def __init__(self, command_format=default_format, category_headings=""):
+        """Creates a new Formatter.
+
+        The replaced keys for the format are: \n
+        `name` ==> The name \n
+        `description` ==> The description \n
+        `usage` ==> The usage \n
+        `credits` ==> The credits \n
+        `category` ==> The category \n
+
+        :param command_format: The format of the command.
+        :param category_headings: The format for category headings.
+        """
+        self._command_format = command_format
+        self.category_headings = category_headings
 
     def format_commands(self, command_descriptions):
         for command in command_descriptions:
@@ -23,7 +39,15 @@ class Formatter(object):
         description = _get_or_default(command, "description", "N/A")
         usage = _get_or_default(command, "usage", "N/A")
         command_credits = _get_or_default(command, "credits", "N/A")
-        parent_folder = _get_or_default(command, "parent_folder", "N/A")
+        category = _get_or_default(command, "parent_folder", "N/A")
+
+        if self.category_headings:
+            if not hasattr(self, "last_category") \
+                  or category != self.last_category:
+                self.last_category = category
+                print(
+                      self.category_headings.format(category=category)
+                )
 
         new_format = []
         for line in self._command_format.splitlines():
@@ -33,12 +57,12 @@ class Formatter(object):
                 line = line["<if>".__len__():-1]
             new_format.append(line)
 
-        new_format = "\n".join(new_format)
+        new_format = "  \n".join(new_format)
         return new_format.format(name=name,
                                  description=description,
                                  usage=usage,
                                  credits=command_credits,
-                                 parent_folder=parent_folder)
+                                 category=category)
 
 
 def _should_remove(line, command):
